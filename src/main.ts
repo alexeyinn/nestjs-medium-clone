8f113c15d3553625e89ded8ca2cfa7ef2e90ebc4
alexeyinn <alexey.i.nn@gmail.com>
2022-05-05 13:10:55 +0300

    feat: db connected

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 if (!process.env.IS_TS_NODE) {
-  require('module-alias/register');
+  require("module-alias/register");
 }
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@app/app.module';
-// 2-6
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "@app/app.module";
+// 2-9
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
