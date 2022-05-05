f038fac0ad31ce98c7c25b5dc26709a03debdb62
alexeyinn <alexey.i.nn@gmail.com>
2022-05-05 11:24:53 +0300

    refactoring: absolute paths

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+if (!process.env.IS_TS_NODE) {
+  require('module-alias/register');
+}
 
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@app/app.module';
+// 2-6
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
