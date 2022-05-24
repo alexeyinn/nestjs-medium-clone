069ee2d9f479f130b0071c96802c3a0ab9cb2996
alexeyinn <alexey.i.nn@gmail.com>
2022-05-24 16:05:32 +0300

    feat: articles dislike

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@app/app.module";
-// 4-29
+// 4-30 11:27
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
