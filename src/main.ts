8ecfb7f6891b8dcb37b83ae8166ce4f73b95cb4e
alexeyinn <alexey.i.nn@gmail.com>
2022-05-26 10:44:15 +0300

    feat: profile following

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@app/app.module";
-// 4-32
+// 4-33
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,7 +1,8 @@
 import { User } from "@app/user/decorators/user.decorator";
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { ProfileService } from "@app/profile/profile.service";
 import { ProfileResponseInterface } from "@app/profile/types/profileResponse.interface";
+import { AuthGuard } from "@app/user/guards/auth.guard";
 
 @Controller("profile")
 export class ProfileController {
@@ -13,6 +14,19 @@ export class ProfileController {
     @Param("userName") profileUsername: string
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(":userName/follow")
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User("id") currentUserId: number,
+    @Param("userName") profileUsername: string
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
       currentUserId,
       profileUsername
     );
