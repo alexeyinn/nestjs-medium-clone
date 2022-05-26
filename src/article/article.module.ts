bb5d814b6ddeabf68f0d5888d8bdff4447b2ac4b
alexeyinn <alexey.i.nn@gmail.com>
2022-05-26 13:29:36 +0300

    feat: feed

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -4,9 +4,12 @@ import { ArticleController } from "@app/article/article.controller";
 import { ArticleEntity } from "@app/article/article.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserEntity } from "@app/user/user.entity";
+import { FollowEntity } from "@app/profile/follow.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ArticleEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ArticleEntity, UserEntity, FollowEntity]),
+  ],
   providers: [ArticleService],
   controllers: [ArticleController],
 })
